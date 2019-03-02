6be5eeb0f4bc6ab8d43668c365041c83c4e8ce6e
Ankita Nagpal <ankitanagpal111@gmail.com>
2019-03-01 20:09:59 -0500

    auth files

---
+++ public/signin/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_44NMEbdCx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2p2iov8tbl3r7tm9of5fonjthe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://kk4nbsy1ef.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
